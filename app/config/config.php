11f9ae6cde0319c1f8d71bd97002a84497340c19
BoonruksaBL036 <boonruksawinanon1234@gmail.com>
2025-12-08 01:18:08 +0700

    change config

--- app/config/config.php
+++ app/config/config.php
@@ -2,9 +2,9 @@
     return[
         'app_name' => 'Student Activity Report',
         'db_host' => 'sql213.infinityfree.com',
-        'db_user' => 'if0_40621053',
-        'db_pass' => 'jXDlgG4zz7kIC2u',
-        'db_name' => 'if0_40621053_student_report_db',
+        'db_user' => 'root',
+        'db_pass' => '',
+        'db_name' => 'student_report_db',
 
     ]
 ?>
